21df9c60bc4db4e5da8cae5454067e9150d51936
xiety <qyyzxty@icloud.com>
2025-05-20 22:42:29 +0800

    fix

--- script/img-write-linux.sh
+++ script/img-write-linux.sh
@@ -1,20 +1,20 @@
-# if [ -f "disk1.vhd" ]; then
-#     mv disk1.vhd disk1.img
-# fi
+if [ -f "disk1.vhd" ]; then
+    mv disk1.vhd disk1.img
+fi
 
-# if [ ! -f "disk1.img" ]; then
-#     echo "error: no disk1.vhd, download it first!!!"
-#     exit -1
-# fi
+if [ ! -f "disk1.img" ]; then
+    echo "error: no disk1.vhd, download it first!!!"
+    exit -1
+fi
 
-# if [ -f "disk2.vhd" ]; then
-#     mv disk2.vhd disk2.img
-# fi
+if [ -f "disk2.vhd" ]; then
+    mv disk2.vhd disk2.img
+fi
 
-# if [ ! -f "disk2.img" ]; then
-#     echo "error: no disk2.vhd, download it first!!!"
-#     exit -1
-# fi
+if [ ! -f "disk2.img" ]; then
+    echo "error: no disk2.vhd, download it first!!!"
+    exit -1
+fi
 
 export DISK1_NAME=disk1.img
 
@@ -25,7 +25,7 @@ dd if=boot.bin of=$DISK1_NAME bs=512 conv=notrunc count=1
 dd if=loader.bin of=$DISK1_NAME bs=512 conv=notrunc seek=1
 
 # 写kernel区，定位到磁盘第100个块
-# dd if=kernel.elf of=$DISK1_NAME bs=512 conv=notrunc seek=100
+dd if=kernel.elf of=$DISK1_NAME bs=512 conv=notrunc seek=100
 
 # 写应用程序init，临时使用
 # dd if=init.elf of=$DISK1_NAME bs=512 conv=notrunc seek=5000
