2519ae8d56e57712d941c294757bd337d8f2ddf4
xiety <qyyzxty@icloud.com>
2025-05-20 23:47:28 +0800

    5.1--利用内联汇编显示字符串

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -1,5 +1,18 @@
 __asm__(".code16gcc");
 
+static void show_msg(const char* msg) {
+    char c;
+    while ((c = *msg++) != '\0') {
+        __asm__ __volatile__ (
+            "mov $0xe, %%ah\n\t"  // 设置 BIOS 中断功能号
+            "int $0x10"
+            :
+            : "a"(c)              // 将当前字符加载到 AL
+        );
+    }
+}
+
 void loader_entry(void) {
+    show_msg("....loading....\n\r");
     for (; ; ) { }
 }
