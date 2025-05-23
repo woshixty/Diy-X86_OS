25048755a10228c6da6dca0aa0a013dad6d095d3
xiety <qyyzxty@icloud.com>
2025-05-24 00:00:10 +0800

    fix

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -5,12 +5,14 @@
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t rv;
-    __asm__ __volatile__ ("inb %[p], %[v]":[v]="a"(rv) : [p]"d"(port));
+    // __asm__ __volatile__ ("inb %1, %0" : "=a"(rv) : "d"(port));
+    __asm__ __volatile__("inb %[p], %[v]" : [v]"=a" (rv) : [p]"d"(port));
     return rv;
 }
 
 static inline void outb(uint16_t port, uint8_t data) {
-    __asm__ __volatile__ ("outb %[v], %[p]":[p]="d"(port) : [v]"a"(data));
+    // __asm__ __volatile__ ("outb %0, %1" : : "a"(data), "Nd"(port));
+    __asm__ __volatile__("outb %[v], %[p]" : : [p]"d" (port), [v]"a" (data));
 }
 
 static inline void cli(void) {
