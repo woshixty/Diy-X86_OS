0854c4f4f51801f86c00aba45bb03bd4adb317b3
xiety <qyyzxty@icloud.com>
2025-05-25 19:27:35 +0800

    5.9--使用LBA模式读取磁盘

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -5,13 +5,17 @@
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t rv;
-    // __asm__ __volatile__ ("inb %1, %0" : "=a"(rv) : "d"(port));
     __asm__ __volatile__("inb %[p], %[v]" : [v]"=a" (rv) : [p]"d"(port));
     return rv;
 }
 
+static inline uint8_t inw(uint16_t port) {
+    uint16_t rv;
+    __asm__ __volatile__("in %[p], %[v]" : [v]"=a" (rv) : [p]"d"(port));
+    return rv;
+}
+
 static inline void outb(uint16_t port, uint8_t data) {
-    // __asm__ __volatile__ ("outb %0, %1" : : "a"(data), "Nd"(port));
     __asm__ __volatile__("outb %[v], %[p]" : : [p]"d" (port), [v]"a" (data));
 }
 
