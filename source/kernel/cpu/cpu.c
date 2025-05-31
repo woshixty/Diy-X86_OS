4b0ce37151df2e2eda7038244535be06bba06cfb
xiety <qyyzxty@icloud.com>
2025-05-31 15:29:50 +0800

    6.4--重新加载GDT(1)

--- source/kernel/cpu/cpu.c
+++ source/kernel/cpu/cpu.c
@@ -1,10 +1,16 @@
 #include "cpu/cpu.h"
 #include "os_cfg.h"
+#include "comm/cpu_instr.h"
 
 static segment_desc_t gdt_table[GDT_TABLE_SIZE];
 
 void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr) {
     segment_desc_t* desc = gdt_table + selector / sizeof(segment_desc_t);
+
+    if(limit > 0xFFFFF) {
+        attr |= SEG_G;
+        limit /= 0x1000;
+    }
 
     desc->limit15_0 = limit & 0xFFFF;
     desc->base15_0 = base & 0xFFFF;
@@ -18,6 +24,10 @@ void init_gdt(void) {
         segment_desc_set(i * sizeof(segment_desc_t), 0, 0, 0);
     }
     
+    segment_desc_set(KERNEL_SELECTOR_DS, 0, 0xFFFFFFFF, );
+    segment_desc_set(KERNEL_SELECTOR_CS, 0, 0xFFFFFFFF, );
+
+    lgdt((uint32_t)gdt_table, sizeof(gdt_table));
 }
 
 void cpu_init(void) {
--- source/kernel/include/os_cfg.h
+++ source/kernel/include/os_cfg.h
@@ -3,4 +3,7 @@
 
 #define GDT_TABLE_SIZE      256
 
+#define KERNEL_SELECTOR_CS  (1 * 8)
+#define KERNEL_SELECTOR_DS  (2 * 8)
+
 #endif
