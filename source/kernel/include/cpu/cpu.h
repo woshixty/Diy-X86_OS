9efbba9dcb17fbf7e8090bbc3f3acb699c39aed6
xiety <qyyzxty@icloud.com>
2025-05-31 20:02:40 +0800

    6.7--添加中断门描述符

--- source/kernel/include/cpu/cpu.h
+++ source/kernel/include/cpu/cpu.h
@@ -14,6 +14,15 @@ typedef struct _segment_desc_t
     uint8_t base31_24;
 } segment_desc_t;
 
+typedef struct _gate_desc_t
+{
+    uint16_t offset15_0;
+    uint16_t selector;
+    uint16_t attr;
+    uint16_t offset31_16;
+} gate_desc_t;
+
+
 #pragma pack()
 
 #define SEG_G           (1 << 15)
@@ -33,5 +42,6 @@ typedef struct _segment_desc_t
 
 void cpu_init(void);
 void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr);
+void gate_desc_set(gate_desc_t* desc, uint16_t selector, uint32_t offset, uint16_t attr);
 
 #endif
