29ad353209d3b5a1205c7ded9ca8afd16472058a
xiety <qyyzxty@icloud.com>
2025-06-01 10:29:17 +0800

    6.15--处理其他类型的异常(1)

--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -1,7 +1,26 @@
 #ifndef IRQ_H
 #define IRQ_H
 
-#define IRQ0_DE 0
+// 中断号码
+#define IRQ0_DE             0
+#define IRQ1_DB             1
+#define IRQ2_NMI            2
+#define IRQ3_BP             3
+#define IRQ4_OF             4
+#define IRQ5_BR             5
+#define IRQ6_UD             6
+#define IRQ7_NM             7
+#define IRQ8_DF             8
+#define IRQ10_TS            10
+#define IRQ11_NP            11
+#define IRQ12_SS            12
+#define IRQ13_GP            13
+#define IRQ14_PF            14
+#define IRQ16_MF            16
+#define IRQ17_AC            17
+#define IRQ18_MC            18
+#define IRQ19_XM            19
+#define IRQ20_VE            20
 
 typedef struct _exception_frame_t
 {
