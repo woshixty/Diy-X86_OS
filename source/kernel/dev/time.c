7c6221f3f64c5dee23c84b19b813f9e4e15f3248
xiety <qyyzxty@icloud.com>
2025-06-01 14:30:00 +0800

    6.21--启动定时器并打开中断(1)

---
+++ source/kernel/dev/time.c
@@ -0,0 +1,22 @@
+#include "dev/time.h"
+#include "comm/types.h"
+#include "comm/cpu_instr.h"
+#include "os_cfg.h"
+#include "cpu/irq.h"
+
+static uint32_t sys_tick;
+
+static void init_pit(void) {
+    uint32_t reload_count = PIT_OSC_FREQ * OS_TICKS_MS / 1000;
+    outb(PIT_COMMAND_MODE_PORT, PIT_CHANNEL | PIT_LOAD_LOHI | PIT_MODE3);
+    outb(PIT_CHANNEL0_DATA_PORT, reload_count & 0xFF);
+    outb(PIT_CHANNEL0_DATA_PORT, (reload_count >> 8) & 0xFF);
+
+    irq_install(IRQ0_TIMER, (irq_handler_t)exception_handler_time);
+    irq_enable(IRQ0_TIMER);
+}
+
+void time_init(void) {
+    sys_tick = 0;
+    init_pit();
+}
