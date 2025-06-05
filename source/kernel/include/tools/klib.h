87076c4ecb484037f92eb3f51ddcac3147310d7b
xiety <qyyzxty@icloud.com>
2025-06-05 23:36:41 +0800

    7.6--实现字符串的格式化输出(1)

--- source/kernel/include/tools/klib.h
+++ source/kernel/include/tools/klib.h
@@ -1,6 +1,7 @@
 #ifndef KLIB_H
 #define KLIB_H
 
+#include <stdarg.h>
 #include "comm/types.h"
 
 void kernel_strcpy(char* dest, const char* src);
@@ -12,4 +13,5 @@ void kernel_memcpy(void* dest, void* src, int size);
 void kernel_memset(void* dest, uint8_t v, int size);
 int kernel_memcmp(void* d1, void* d2, int size);
 
+void kernel_vsprintf(char* buf, const char* fmt, va_list args);
 #endif
