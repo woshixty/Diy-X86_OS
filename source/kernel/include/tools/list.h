9b8b031df6bd98b147feb2267e8e04f63d90462e
xiety <qyyzxty@icloud.com>
2025-06-14 18:59:45 +0800

    9.6--获取节点所在的结构(2)

--- source/kernel/include/tools/list.h
+++ source/kernel/include/tools/list.h
@@ -1,6 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include "comm/types.h"
+
 typedef struct _list_node_t
 {
     struct _list_node_t* pre;
@@ -50,5 +52,19 @@ void list_insert_last(list_t* list, list_node_t* node);
 
 list_node_t* list_remove_first(list_t* list);
 list_node_t* list_remove(list_t* list, list_node_t* node);
+
+// 1.求结点在所在结构中的偏移:定义一个指向0的指针，用(struct aa *)&0->node，所得即为node字段在整个结构体的偏移
+#define offset_in_parent(parent_type, node_name)    \
+    ((uint32_t)&(((parent_type*)0)->node_name))
+
+// 2.求node所在的结构体首址：node的地址 - node的偏移
+// 即已知a->node的地址，求a的地址
+#define offset_to_parent(node, parent_type, node_name)   \
+    ((uint32_t)node - offset_in_parent(parent_type, node_name))
+
+// 3. 进行转换: (struct aa *)addr
+// list_node_parent(node_addr, struct aa, node_name)
+#define list_node_parent(node, parent_type, node_name)   \
+    ((parent_type *)(node ? offset_to_parent((node), parent_type, node_name) : 0))
 
 #endif
