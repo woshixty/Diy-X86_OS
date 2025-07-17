7bbf60ab55fb04ee2cc8cbf56870cc0fd3838ad2
xiety <qyyzxty@icloud.com>
2025-07-17 23:28:41 +0800

    10.2--将任务加入到就绪队列

--- source/kernel/include/core/task.h
+++ source/kernel/include/core/task.h
@@ -5,17 +5,31 @@
 #include "cpu/cpu.h"
 #include "tools/list.h"
 
+#define TASK_NAME_SIZE 32
+
 /**
  * @brief 任务控制块结构
  */
 typedef struct _task_t {
 	//uint32_t * stack;
+	enum {
+		TASK_CREATED,
+		TASK_RUNNING,
+		TASK_SLEEP,
+		TASK_READY,
+		TASK_WAITING,
+	}state;
+
+	char name[TASK_NAME_SIZE];
+
+	list_node_t run_node;
+	list_node_t all_node;
 
 	tss_t tss;				// 任务的TSS段
 	uint16_t tss_sel;		// tss选择子
 }task_t;
 
-int task_init (task_t *task, uint32_t entry, uint32_t esp);
+int task_init (task_t *task, const char* name, uint32_t entry, uint32_t esp);
 void task_switch_from_to (task_t * from, task_t * to);
 
 typedef struct _task_manager_t
@@ -31,5 +45,7 @@ typedef struct _task_manager_t
 void task_manager_init (void);
 void task_first_init (void);
 task_t* task_first_task (void);
+void task_set_ready (task_t* task);
+void task_set_block (task_t* task);
 
 #endif
