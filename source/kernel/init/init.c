1fde642e3f81ebec83242aa577c9f012a9aeb20b
xiety <qyyzxty@icloud.com>
2025-07-17 22:40:17 +0800

    10.1--添加任务管理器

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -23,9 +23,10 @@ void kernel_init (boot_info_t * boot_info) {
     log_init();
     irq_init();
     time_init();
+
+    task_manager_init();
 }
 
-static task_t first_task;       // 第一个任务
 static uint32_t init_task_stack[1024];	// 空闲任务堆栈
 static task_t init_task;
 
@@ -38,94 +39,22 @@ void init_task_entry(void) {
 
     for (;;) {
         log_printf("init task: %d", count++);
-        task_switch_from_to(&init_task, &first_task);
+        task_switch_from_to(&init_task, task_first_task());
     }
-}
-
-void list_test (void) {
-    list_t list;
-    list_node_t nodes[5];
-
-    list_init(&list);
-
-    for (int i = 0; i < 5; i++) {
-        list_node_t* node = nodes + i;
-
-        log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
-        list_insert_first(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d",
-        list_first(&list), list_last(&list), list_count(&list));
-
-    list_init(&list);
-    for (int i = 0; i < 5; i++) {
-        list_node_t* node = nodes + i;
-
-        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
-        list_insert_last(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d",
-        list_first(&list), list_last(&list), list_count(&list));
-
-    // remove first
-    for (int i = 0; i < 5; i++)
-    {
-        list_node_t* node = list_remove_first(&list);
-        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d",
-        list_first(&list), list_last(&list), list_count(&list));
-    
-    // insert last
-    for (int i = 0; i < 5; i++) {
-        list_node_t* node = nodes + i;
-
-        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
-        list_insert_last(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d",
-        list_first(&list), list_last(&list), list_count(&list));
-
-    // remove node
-    for (int i = 0; i < 5; i++)
-    {
-        list_node_t* node = nodes + i;
-        log_printf("remove from list: %d, 0x%x", i, (uint32_t)node);
-        list_remove(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d",
-        list_first(&list), list_last(&list), list_count(&list));
-
-    struct type_t {
-        int i;
-        list_node_t node;
-    }v = {0x123456};
-
-    list_node_t * v_node = &v.node;
-    struct type_t * p = list_node_parent(v_node, struct type_t, node);
-    if (p->i != 0x123456) {
-        log_printf("error");
-    }
-    
 }
 
 void init_main(void) {
-    list_test();
-
     log_printf("Kernel is running....");
     log_printf("Version: %s, name: %s", OS_VERSION, "tiny x86 os");
     log_printf("%d %d %x %c", -123, 123456, 0x12345, 'a');
 
     // 初始化任务
     task_init(&init_task, (uint32_t)init_task_entry, (uint32_t)&init_task_stack[1024]);
-    task_init(&first_task, 0, 0);
-    write_tr(first_task.tss_sel);
+    task_first_init();
 
-    // int a = 3 / 0;
-    // irq_enable_global();
     int count = 0;
     for (;;) {
         log_printf("first task: %d", count++);
-        task_switch_from_to(&first_task, &init_task);
+        task_switch_from_to(task_first_task(), &init_task);
     }
 }
