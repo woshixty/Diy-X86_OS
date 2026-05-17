2a7a9a787ffb8a86deea7a8581ef26221bdc8b6d
xiety <qyyzxty@icloud.com>
2026-05-18 00:26:26 +0800

    13.3--隔离操作系统与进程(3)-搬运代码

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -11,19 +11,16 @@
 #include "ipc/sem.h"
 #include "core/memory.h"
 
-static boot_info_t * init_boot_info;        // 启动信息
-
 /**
  * 内核入口
  */
 void kernel_init (boot_info_t * boot_info) {
-    init_boot_info = boot_info;
-
     // 初始化CPU，再重新加载
     cpu_init();
-
-    memory_init(init_boot_info);
     log_init();
+
+    memory_init(boot_info);
+
     irq_init();
     time_init();
 
